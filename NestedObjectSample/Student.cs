be6c73d9d7d1146d9ed16d9950e06c0f41de4b53
Dennis Kafton <boom1axe@att.net>
2019-07-15 13:44:20 -0700

    Created Student Class

---
+++ NestedObjectSample/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjectSample
+{
+    class Student
+    {
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public string HomeAddress { get; set; }
+
+        /// <summary>
+        /// The Students Home Phone Number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+    }
+}
