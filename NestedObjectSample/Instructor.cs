582e84fcbb99c7450e02b852b578bf2a3495972d
Dennis Kafton <boom1axe@att.net>
2019-07-17 13:28:40 -0700

    Added Instructor class

---
+++ NestedObjectSample/Instructor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjectSample
+{
+    class Instructor
+    {
+        public Instructor()
+        {
+            CourseLoad = new List<Course>();
+        }
+
+        /// <summary>
+        /// The official @cptc.edu work email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// The location of the office in building/room format
+        /// ex. B11R112
+        /// </summary>
+        public string OfficeLocation { get; set; }
+
+        /// <summary>
+        /// The four digit extension
+        /// </summary>
+        public string OfficePhone { get; set; }
+
+        /// <summary>
+        /// The first then last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// The courses the instructor is currently teaching
+        /// </summary>
+        public List<Course> CourseLoad { get; set; }
+
+        public void AddCourse(Course c)
+        {
+            if(CourseLoad == null)
+            {
+                CourseLoad = new
+            }
+        }
+    }
+}
